d81a7df64f5859efd6dcfb7f0e83a623d3d7edd4
apensoneault <andrew-pensoneault@uiowa.edu>
2020-05-17 11:23:56 -0500

    added io for python

--- asynch_py/io.py
+++ asynch_py/io.py
@@ -7,13 +7,11 @@ def read_json(filename):
         data = json.load(f)
     return data
 
-def read_asynch_params(filename):
-   """Reads parameter json and replaces functions with respective function"""
-    data = read_json(filename):
-
+def process_asynch_params(data):
     assim_params = data['assim_params']
     measure_params = data['measure_params']    
-    perturb_params = data['perturb_params'] 
+    var_perturb_params = data['var_perturb_params'] 
+    param_perturb_params = data['param_perturb_params'] 
 
     if data['assim_type'] == 'particle':
     	assim = particle(assim_params)
@@ -21,17 +19,62 @@ def read_asynch_params(filename):
         assim = enkf(assim_params)
     elif data['assim_type'] == 'no_assimilate':
         assim = no_assimilate(assim_params)
+    else:
+        raise ValueError('Invalid assim_type')
 
-    if data['measure_type'] == 'links':
-    	assim = link(measure_params)
+    for i in range(len(data['measure_type'])):
+        if data['measure_type'][i] == 'links':
+            assim = link(measure_params[i])
+        elif data['measure_type'][i] == 'soil_moisture':
+            assim = soil_moisture(measure_params[i])
+        else:
+            raise ValueError('Invalid measure_type')
 
-   for i in range(len(data['perturb_type'])
-       if data['perturb_type'][i] == 'percent':
-           assim = percent(assim_params)
-       elif data['perturb_type'][i] == 'absolute':
-           assim = absolute(assim_params)
-       elif data['perturb_type'][i] == 'variable_percent':
-           assim = variable_percent(assim_params) 
-       elif data['perturb_type'][i] == 'variable_absolute':
-           assim = variable_absolute(assim_params) 
-        
+    for i in range(len(data['var_perturb_type'])):
+        if data['perturb_type'][i] == 'percent':
+            assim = percent(assim_params[i])
+        elif data['perturb_type'][i] == 'absolute':
+            assim = absolute(assim_params[i])
+        elif data['perturb_type'][i] == 'per_variable_percent':
+            assim = variable_percent(assim_params[i]) 
+        elif data['perturb_type'][i] == 'per_variable_absolute':
+            assim = variable_absolute(assim_params[i]) 
+        else:
+            raise ValueError('Invalid var_perturb_type')
+
+
+    for i in range(len(data['param_perturb_type'])):
+        if data['perturb_type'][i] == 'percent':
+            assim = percent(assim_params[i])
+        elif data['perturb_type'][i] == 'absolute':
+            assim = absolute(assim_params[i])
+        elif data['perturb_type'][i] == 'per_variable_percent':
+            assim = variable_percent(assim_params[i]) 
+        elif data['perturb_type'][i] == 'per_variable_absolute':
+            assim = variable_absolute(assim_params[i]) 
+        else:
+            raise ValueError('Invalid param_perturb_type')
+
+def get_model_params(data):
+    """ File which reads initial condition to get model parameters (only ini for now, will expand)"""
+    ini_filename = data['init_cond']
+    with open(ini_filename, "r") as f:
+       counter = 0
+       while counter < 5: #the first 5 line of the ini folder contain all relevent data
+          line = f.read(1)
+          if not line.strip(): 
+              counter += 1
+              if counter == 1:
+                  data['model_num'] = int(line)
+              elif counter == 2:
+                  data['link_num'] = int(line)
+              elif counter == 5:
+                  data['link_var_num'] = len(list(map(int, line.split(" ")))
+
+
+def get_asynch_params(filename):
+   """Reads parameter json and replaces function names with respective function"""
+    data = read_json(filename)
+    data = process_asynch_params(data)
+    data = get_model_params(data)
+    return data
--- asynchdist_assim.py
+++ asynchdist_assim.py
@@ -2,69 +2,52 @@ from asynch_py.asynch_interface import asynchsolver
 import sys
 from mpi4py import MPI
 import numpy as np
+buffer_size = 20 #size of one sprintf entry for output
 
 ## Parse command line arguments
 numargs = len(sys.argv)
 if numargs != 2:
-	print 'Need an input .gbl file'
+	print 'Need an input .json file'
 	sys.exit(1)
 
 ## Make a json file that gets initial numbers
-#get_asynch_params()
-
 #Should contain the following context
 #1). Number of steps in a model run
 #2). How long one step will be
-#3). Start and stop time window
-#4). filename of a txt file of the form of a gbl file missing the following:
+#3). Initial Global Parameters
+#4). Initial initial Condition
+#5). Start and stop time window
+#6). filename of a txt file of the form of a gbl file missing the following:
 #sav
 #str
 #rvr
 #prm
 #output
-#5-6). filename for rvr and prm 
-#7). Source for rainfall 
-#8). Source for evaporation
-#9). Method for Assimilation
-#10). Respective Assimilation Parameters
-#11). List of Method for Perturbation
-#12). Respective Perturbation Parameters
-#13). Method for Measurement
-#14). Respective Measurement Parameters
-#15). Output Folder
+#7-8). filename for rvr and prm 
+#9). Source for rainfall 
+#10). Source for evaporation
+#11). Method for Assimilation
+#12). Respective Assimilation Parameters
+#13). List of Method for Perturbation
+#14). Respective Perturbation Parameters
+#15). Method for Measurement
+#16). Respective Measurement Parameters
+#17). Output Folder
 
+asynch_data = get_asynch_params(sys.argv[1])
+import pdb; pdb.set_trace()
+#forcing_data = get_forcings()
+#measure_data = get_meas()
 
-
-
-
-
-
-
-
-
-
-
-
-buffer_size = 20
-num_steps = 48
-num_links = 3202
-link_vars = 4
-t_steps = 50
-ens_num = 1
-param_init = np.array([0.33, 0.20, -0.1, 0.02, 2.0425e-6, 0.02, 0.5, 0.10, 0.0, 99.0, 3.0])     
 large_string_len = buffer_size*num_steps*num_links*num_vars
-num_param = len(param_init)
-
 ens_bg = np.zeros((link_vars*num_steps*num_links+num_param,ens_num))
-#get_forcings()
-
 for i in range(tsteps):
-#    get_current_forcings()
+#    forcings = get_current_forcings(forcing_data)
     for n in ens_to_assim:
-#        make_files(n) 
+#        make_files(n,asynch_data,forcings) 
         if i==0:
             asynch = asynchsolver()
-            asynch.Parse_GBL(sys.argv[1] + str(n) + '.gbl')
+            asynch.Parse_GBL(data['tmp_folder'] + str(n) + '.gbl')
             asynch.Load_Network()
             asynch.Partition_Network()
             asynch.Load_Network_Parameters(False)
@@ -95,8 +78,9 @@ for i in range(tsteps):
         outarray = np.fromstring(output_string,sep=',')[:-1] #removes extra comma introduced in writing csv
         outvec = np.reshape(outarray,(numsteps+1,-1))[1:,:].flatten()
         state = np.concatenate((outvec,parameters))
-#        state += perturb(state)
+#        state += perturb(state,asynch_data)
         ens_bg[:,n] = state
-#    ens_anal = assimilate()
-#    write_results() 
+#    measure = get_meas(asynch_data)
+#    ens_anal = assimilate(state,asynch_data)
+#    write_results(asynch_data) 
 #remove_files()
